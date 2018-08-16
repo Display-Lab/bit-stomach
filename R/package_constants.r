479577c7651119bcc3442112ddc458657535f0e1
Colin Gross <grosscol@gmail.com>
2018-08-16 12:10:57 -0400

    Update count with current gap terminology. Update package constants with current terminology.

--- R/package_constants.r
+++ R/package_constants.r
@@ -7,12 +7,9 @@ BS$DEFAULT_COL_SPEC <- list(
   "ordering_cols" = c("timepoint")
 )
 
-BS$DEFAULT_APP_ONTO_URL <- "https://inference.es/app/onto#"
-BS$HAS_PERFORMERS_URI <- "http://purl.obolibrary.org/obo/fio#HasPerformer"
-BS$PERFORMER_URI <- "http://purl.obolibrary.org/obo/fio#FIO_0000001"
-BS$SITUATION_URI <- "http://purl.obolibrary.org/obo/fio#FIO_0000050"
-BS$HAS_PART_URI <- "http://purl.obolibrary.org/obo/bfo#BFO_0000051"
+BS$DEFAULT_APP_ONTO_URL <- "http://example.com/app#"
+BS$HAS_PERFORMERS_URI <- "http://example.com/slowmo#slowmo_0000001"
+BS$PERFORMER_URI <- "http://example.com/slowmo#ascribee"
 BS$HAS_DISPOSITION_URI <- "http://purl.obolibrary.org/obo/RO_0000091"
-BS$CLIENT_SITUATION_URI <- "https://inference.es/app/onto#Client_Situation"
 
 BS$DEFAULT_VERBOSE <- FALSE
