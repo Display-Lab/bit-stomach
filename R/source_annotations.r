c80999b3bf6ff1a6e56572bde3b825c332a62b1c
Colin Gross <grosscol@gmail.com>
2019-02-07 09:57:39 -0500

    Clarify use of annotation sourcing.

--- R/source_annotations.r
+++ R/source_annotations.r
@@ -3,6 +3,7 @@
 #' @description Source the annotations.r file which contain the fucntions to apply to the
 #'   data in order to make inferences about performers from the data.
 #' @param path Path too annotations R file.
+#' @return An environment with the evaluated the contents of the annotations.R
 #' @note Annotation functions will be passed two arguements: data and col_spec
 #'   The col_spec is a list of three character vectors: id_cols, ordering_cols, perf_cols
 source_annotations <- function(path) {
