269cbdbdd2be0b69e768e54ffb9f35f7e97cb9da
Colin Gross <grosscol@gmail.com>
2019-04-16 11:30:19 -0400

    Bake in support for causal pathway characteristics. Provide slowmo disposition URIs for annotations.

--- R/package_constants.r
+++ R/package_constants.r
@@ -1,19 +1,6 @@
 # top level config environment
 BS <- new.env()
 
-# Used in swap_in_uris.r as lookup for shortname names in annotations to full IRI
-BS$DEFAULT_URI_LOOKUP <- list(
-  performer            = "http://purl.obolibrary.org/obo/psdo_0000085",
-  has_performer        = "http://example.com/slowmo#IsAboutPerformer",
-  uses_template        = "http://example.com/slowmo#IsAboutTemplate",
-  spek                 = "http://example.com/slowmo#spek",
-  normative_comparator = "http://example.com/slowmo#NormativeComparator",
-  related_location     = "http://example.com/slowmo#RelatedLocation",
-  has_part             = "http://purl.obolibrary.org/obo/bfo#BFO_0000051",
-  has_disposition      = "http://purl.obolibrary.org/obo/RO_0000091",
-  client_spek          = "http://example.com/app#clientname_spek"
-)
-
 # IRIs for parsing spek
 BS$HAS_DISPOSITION_URI <- "http://purl.obolibrary.org/obo/RO_0000091"
 BS$DEFAULT_APP_ONTO_URL <- "http://example.com/app#"
@@ -28,6 +15,33 @@ BS$COLUMNS_IRI <- "http://www.w3.org/ns/csvw#columns"
 BS$TABLE_IRI <- "http://www.w3.org/ns/csvw#Table"
 BS$DIALECT_IRI <- "http://www.w3.org/ns/csvw#dialect"
 BS$COL_NAME_IRI <- "http://www.w3.org/ns/csvw#name"
+
+# Slowmo ascribee IRIs
+BS$CAPABILITY_BARRIER <- "http://example.com/slowmo#CapabilityBarrier"
+BS$NEGATIVE_TREND     <- "http://example.com/slowmo#NegativePerformanceTrend"
+BS$POSITIVE_TREND     <- "http://example.com/slowmo#PositivePerformanceTrend"
+BS$NEGATIVE_GAP       <- "http://example.com/slowmo#NegativePerformanceGap"
+BS$POSITIVE_GAP       <- "http://example.com/slowmo#PositivePerformanceGap"
+BS$PERFORMANCE_GAP    <- "http://example.com/slowmo#PerformanceGap"
+
+# Used in swap_in_uris.r as lookup for shortname names in annotations to full IRI
+BS$DEFAULT_URI_LOOKUP <- list(
+  performer            = "http://purl.obolibrary.org/obo/psdo_0000085",
+  has_performer        = BS$HAS_PERFORMER_URI,
+  uses_template        = "http://example.com/slowmo#IsAboutTemplate",
+  spek                 = "http://example.com/slowmo#spek",
+  normative_comparator = "http://example.com/slowmo#NormativeComparator",
+  related_location     = "http://example.com/slowmo#RelatedLocation",
+  has_part             = "http://purl.obolibrary.org/obo/bfo#BFO_0000051",
+  has_disposition      = "http://purl.obolibrary.org/obo/RO_0000091",
+  client_spek          = "http://example.com/app#clientname_spek",
+  capability_barrier   = BS$CAPABILITY_BARRIER,
+  negative_trend       = BS$NEGATIVE_TREND,
+  positive_trend       = BS$POSITIVE_TREND,
+  negative_gap         = BS$NEGATIVE_GAP,
+  positive_gap         = BS$POSITIVE_GAP,
+  performance_gap      = BS$PERFORMANCE_GAP
+)
 
 # Default configuration
 BS$DEFAULT_OUTFILE <- ""
