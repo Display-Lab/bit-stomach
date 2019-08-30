5cde4877f492798c95cd4de52a43650bf4020205
Colin Gross <grosscol@gmail.com>
2019-08-30 17:03:45 -0400

    Prepare for annotation with respect to measurement   Provide function to split data into list by measure id

---
+++ tests/testthat/test_split_by_measure.r
@@ -0,0 +1,27 @@
+library(spekex)
+context('Test split by measure')
+
+VA2_SPEK <- spekex::read_spek(spekex::get_spek_path("va2"))
+VA2_DATA <- read_data(spekex::get_data_path("va2"))
+
+SHAM_SPEK <- spekex::read_spek(spekex::get_spek_path("sham"))
+SHAM_DATA <- read_data(spekex::get_data_path("sham"))
+
+test_that('Split by measure yields correct size list.', {
+  result <- split_by_measure(VA2_DATA, VA2_SPEK)
+  expect_equal(length(result), 2)
+})
+
+test_that('Split by measure yields list using measure ids as names.', {
+  result <- split_by_measure(VA2_DATA, VA2_SPEK)
+  measure_ids <- c("_:m1", "_:m2")
+  expect_setequal(names(result), measure_ids)
+})
+
+test_that('No measure column returns list with only original data', {
+  result <- split_by_measure(SHAM_DATA, SHAM_SPEK)
+  expect_identical(names(result), c("_:m1"))
+  expect_equivalent(result[[1]], SHAM_DATA)
+})
+
+
