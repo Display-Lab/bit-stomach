5977c12d9d1e12e2601f2c3da4aa80f71e072aaf
Colin Gross <grosscol@gmail.com>
2019-09-09 09:59:23 -0400

    Provide instructions to configure test settings.

--- tests/testthat/setup_integration.R
+++ tests/testthat/setup_integration.R
@@ -1,7 +1,9 @@
 # Setup integration test switch
 
-#Default to FULLTEST being true
+# Default to FULLTEST being true
 if(is.na( Sys.getenv("FULLTEST",unset=NA))) { Sys.setenv("FULLTEST"=TRUE) }
+# Sys.setenv(FULLTEST=FALSE) # Use this line to disable fulltest
 
-#Default to BS_VERBOSE being FALSE
-if(is.na( Sys.getenv("FULLTEST",unset=NA))) { Sys.setenv("BS_VERBOSE"=FALSE) }
+# Default to BS_VERBOSE being FALSE
+if(is.na( Sys.getenv("BS_VERBOSE",unset=NA))) { Sys.setenv("BS_VERBOSE"=FALSE) }
+# Sys.setenv(BS_VERBOSE=TRUE) # Use this line to enable verbose
